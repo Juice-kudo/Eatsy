985411028deb2626398933863ba1c033f567a945
Juice-kudo <hani6908@me.com>
2021-12-23 19:30:54 +0900

    Initial Commit

---
+++ Great/GreatApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreatApp.swift
+//  Great
+//
+//  Created by Lee Juwon on 2021/12/23.
+//
+
+import SwiftUI
+
+@main
+struct GreatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
